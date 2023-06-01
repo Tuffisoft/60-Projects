efda21046ce1bea140abc0159ec077871b2088be
Fi <vancetti@googlemail.com>
2023-06-01 18:03:46 +0200

    "start with pomodoro"

---
+++ 06. Pomodoro/index.js
@@ -0,0 +1,11 @@
+const timerEL = document.getElementById("timer");
+const startEl = document.getElementById("start");
+const pauseEl = document.getElementById("pause");
+const resetEl = document.getElementById("reset");
+
+function startTimer () {
+    
+}
+
+
+startEl.addEventListener("click", startTimer)
