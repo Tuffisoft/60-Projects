7d0c9407ae50528f9ae5e1a17eb375f7d976e8d1
Fi <vancetti@googlemail.com>
2023-06-01 10:37:56 +0200

    "recipe book app finished"

--- 04. Recipe Book App/index.js
+++ 04. Recipe Book App/index.js
@@ -1,6 +1,31 @@
-const API_KEY = "593f531d2ec24a5c969870c1d76b3764"
+const API_KEY = "593f531d2ec24a5c969870c1d76b3764";
+const recipeListEl = document.getElementById("recipe-list");
 
-async function getRecipes() {
+
+function displayRecipes (recipes) {
+    recipeListEl.innerHTML = "";
+    recipes.forEach((recipe )=> {
+        const recipeItemEl = document.createElement("li");
+        recipeItemEl.classList.add("recipe-item");
+        recipeImageEl = document.createElement("img");
+        recipeImageEl.src = recipe.image;
+
+        recipeTitleEl = document.createElement("h2");
+        recipeTitleEl.innerText = recipe.title;
+        recipeIngredientsEl = document.createElement("p")
+        recipeIngredientsEl.innerHTML = `
+        <strong>Ingredients: </strong> ${recipe.extendedIngredients
+            .map((ingredient) => ingredient.original)
+            .join(", ")}`
+        
+        recipeItemEl.appendChild(recipeImageEl);
+        recipeItemEl.appendChild(recipeTitleEl);
+        recipeItemEl.appendChild(recipeIngredientsEl)
+        recipeListEl.appendChild(recipeItemEl);
+    });
+}
+
+async function getRecipes () {
     const response = await fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`)
     const data = await response.json()
     return data.recipes
@@ -9,6 +34,7 @@ async function getRecipes() {
 async function init () {
     const recipes = await getRecipes()
     console.log(recipes)
+    displayRecipes(recipes)
 }
 
 init()
