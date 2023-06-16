426e2fda7ec61d9065ca4400b37ea622baea1505
Tuffisoft <vancetti@gmail.com>
2023-06-16 14:20:42 +0200

    "add stop watch"

---
+++ 08. Stop Watch/index.js
@@ -0,0 +1,12 @@
+const timerEL = document.querySelector(".timer");
+const startBtn = document.querySelector(".start");
+const stopBtn = document.querySelector(".stop");
+const resetBtn = document.querySelector(".reset");
+
+function fi () {
+    timerEL.innerHTML = "Hello"
+}
+  
+
+
+startBtn.addEventListener("click", fi)
