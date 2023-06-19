bfc473bda19cb856fc2e7fa2aa9321e9aff267eb
Tuffisoft <vancetti@gmail.com>
2023-06-19 14:57:18 +0200

    "add some work on index.js"

--- 10. Image Search/index.js
+++ 10. Image Search/index.js
@@ -12,12 +12,31 @@ async function searchImages() {
     inputData = searchFieldEl.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${apikey}`
     const response = await fetch(url);
-    const data = await response.json;
-    console.log(data)
+    const data = await response.json();
+    if (page === 1) {
+        searchResultsEl.innerHTML = "";
+    }
+
+    const results = data.results
+
+    const imageWrapper = document.createElement("div")
+    imageWrapper.classList.add("search-result")
+    const image = document.createElement("img")
+    image.src = result.urls.small
+    image.alt = result.alt_description
+    const imageLink = document.createElement("a")
+    imageLink.href = result.links.innerHTML
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description
+
+    if (page > 1) {
+        showMoreEl.style.display = block;
+    }
 }
 
 
 formEl.addEventListener("submit", (e) => {
     e.preventDefault();
+    page = 1;
     searchImages()
 });
