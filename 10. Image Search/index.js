dc9400d2e7e7ef022b416231d9445f5645d8d244
Tuffisoft <vancetti@gmail.com>
2023-06-20 13:26:11 +0200

    "image search app near finished"

--- 10. Image Search/index.js
+++ 10. Image Search/index.js
@@ -1,42 +1,53 @@
 const apikey = "TzxzOhpua5-d6oZe2NgIh5gAqb1CL7Ufqr6UNIyZAOM";
 
-const searchFieldEl = document.getElementById("search-field");
+const searchInputEl = document.getElementById("search-input");
 const formEl = document.querySelector("form");
-const searchResultsEl = document.getElementById("search-results");
+const searchResultsEl = document.querySelector(".search-results");
 const showMoreEl = document.getElementById("show-more")
 
 let inputData = "";
 let page = 1;
 
 async function searchImages() {
-    inputData = searchFieldEl.value;
+    inputData = searchInputEl.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${apikey}`
     const response = await fetch(url);
     const data = await response.json();
-    if (page === 1) {
+    console.log(data)
+    if (page === 1){
         searchResultsEl.innerHTML = "";
     }
-
     const results = data.results
 
-    const imageWrapper = document.createElement("div")
-    imageWrapper.classList.add("search-result")
-    const image = document.createElement("img")
-    image.src = result.urls.small
-    image.alt = result.alt_description
-    const imageLink = document.createElement("a")
-    imageLink.href = result.links.innerHTML
-    imageLink.target = "_blank";
-    imageLink.textContent = result.alt_description
+    results.map((result) => {
+        const imageWrapper = document.createElement("div")
+        imageWrapper.classList.add("search-result")
+        const image = document.createElement("img")
+        image.src = result.urls.small
+        image.alt = result.alt_description
+        const imageLink = document.createElement("a")
+        imageLink.href = result.links.innerHTML
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResultsEl.appendChild(imageWrapper);
+    });
+
+    page++
 
     if (page > 1) {
-        showMoreEl.style.display = block;
+        showMoreEl.style.display = "block";
     }
 }
 
-
-formEl.addEventListener("submit", (e) => {
+    formEl.addEventListener("submit", (e) => {
     e.preventDefault();
     page = 1;
     searchImages()
 });
+
+showMoreEl.addEventListener("click", () => {
+    searchImages();
+})
