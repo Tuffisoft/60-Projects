bdff911bd8052c0c0b12377774822ca391b78a71
Tuffisoft <vancetti@gmail.com>
2023-06-21 10:10:36 +0200

    "tidy dad joke code"

--- 12. Dad Jokes/index.js
+++ 12. Dad Jokes/index.js
@@ -5,31 +5,28 @@ const options = {
     method: "GET",
     headers: {
         "X-Api-Key": apikey,
-    }
-}
+    };
+};
 
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
 
 async function tellMeAJoke () {
 
     try {
-    jokeArea.innerText = "thinking.....";
-    jokeBtn.disabled = true;
-    jokeBtn.innerText = "He is thinking!"
-    const response = await fetch (apiURL, options)
-    const data = await response.json()
+        jokeArea.innerText = "thinking.....";
+        jokeBtn.disabled = true;
+        jokeBtn.innerText = "He is thinking!"
+        const response = await fetch (apiURL, options)
+        const data = await response.json()
 
-    jokeBtn.disabled = false;
-    jokeBtn.innerText = "Tell me a joke!"
+        jokeBtn.disabled = false;
+        jokeBtn.innerText = "Tell me a joke!"
 
-    jokeArea.innerText = data[0].joke
+        jokeArea.innerText = data[0].joke
         
     } catch (error) {
         jokeArea.innerText = "No internet"
-    }
-
-    
-}
-
+    };
+};
 
 jokeBtn.addEventListener ("click", tellMeAJoke);
