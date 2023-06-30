b8c471fa7fd0a5082e528ec31701470b945d60ad
Tuffisoft <vancetti@gmail.com>
2023-06-30 08:59:05 +0200

    "progress made with fifa"

--- Fi/FIFA/index.js
+++ Fi/FIFA/index.js
@@ -1,3 +1,23 @@
-require("dotenv").config();
 
-console.log(process.env.FIFA_API_KEY);
+
+const apikey = "";
+const btnEl = document.getElementById("btn");
+const apiURL = "https://futdb.app/api/players"
+const options = {
+    method: "GET",
+    headers: {
+        "X-AUTH-TOKEN": apikey,
+    }
+};
+
+async function button () {
+    console.log("click")
+    const response = await fetch(apiURL, options);
+    console.log(response)
+    const data = await response.json();
+    console.log(data);
+}
+
+
+
+btnEl.addEventListener("click", button)
